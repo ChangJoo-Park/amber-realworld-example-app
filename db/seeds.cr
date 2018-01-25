9bef5d3d6bc852fcfd113189b0f139d7036ae35a
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-01-26 07:31:39 +0900

    Update seed. add tag to article

--- db/seeds.cr
+++ db/seeds.cr
@@ -16,11 +16,14 @@ Tag.clear
 end
 
 Article.clear
+
+tags = Tag.all
 [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].each do |index|
   article = Article.new
   article.title = Faker::Lorem.sentence
   article.summary = Faker::Lorem.sentence
   article.body = Faker::Lorem.paragraph(4)
   article.user = user
+  article.tag = tags.sample
   article.save
 end
