c06ba4f1ff15f2d66c49a87a65250864de628c70
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-01-25 19:02:54 +0900

    Update seed - user, tag, article

--- db/seeds.cr
+++ db/seeds.cr
@@ -1,3 +1,4 @@
+require "faker"
 require "../config/application.cr"
 
 User.clear
@@ -6,8 +7,17 @@ user.email = "admin@example.com"
 user.password = "password"
 user.save
 
+Tag.clear
+[1, 2, 3, 4, 5].each do |index|
+  tag = Tag.new
+  tag.name = Faker::Lorem.word
+  tag.save
+end
+
 Article.clear
-article = Article.new
-article.title = "HELLO"
-article.body = "BODY"
-article.save
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].each do |index|
+  article = Article.new
+  article.title = Faker::Lorem.sentence
+  article.body = Faker::Lorem.paragraph(4)
+  article.save
+end
