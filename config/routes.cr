e8a3281c906c6e258c6658261691b25b5a641270
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-01-26 22:35:22 +0900

    Add settings routes

--- config/routes.cr
+++ config/routes.cr
@@ -1,6 +1,5 @@
 Amber::Server.configure do |app|
   pipeline :web do
-  
     # Plug is the method to use connect a pipe (middleware)
     # A plug accepts an instance of HTTP::Handler
     plug Amber::Pipe::Error.new
@@ -10,7 +9,7 @@ Amber::Server.configure do |app|
     plug Amber::Pipe::CSRF.new
     # Reload clients browsers (development only)
     plug Amber::Pipe::Reload.new if Amber.env.development?
-  
+
     plug Authenticate.new
   end
 
@@ -27,16 +26,16 @@ Amber::Server.configure do |app|
   end
 
   routes :web do
-      resources "/articles", ArticleController
-      resources "/tags", TagController
-      get "/profile", UserController, :show
-  get "/profile/edit", UserController, :edit
-  patch "/profile", UserController, :update
-  get "/signin", SessionController, :new
-  post "/session", SessionController, :create
-  get "/signout", SessionController, :delete
-  get "/signup", RegistrationController, :new
-  post "/registration", RegistrationController, :create
+    resources "/articles", ArticleController
+    resources "/tags", TagController
+    get "/profile", UserController, :show
+    patch "/profile", UserController, :update
+    get "/signin", SessionController, :new
+    post "/session", SessionController, :create
+    get "/signout", SessionController, :delete
+    get "/signup", RegistrationController, :new
+    post "/registration", RegistrationController, :create
     get "/", HomeController, :index
+    get "/settings", UserController, :edit
   end
 end
