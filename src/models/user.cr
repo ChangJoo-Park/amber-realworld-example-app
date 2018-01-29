a418ac84080f6f0f32a856d85c36266027945701
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-01-29 19:05:43 +0900

    Remove through relationship

--- src/models/user.cr
+++ src/models/user.cr
@@ -15,7 +15,7 @@ class User < Granite::ORM::Base
 
   has_many :articles
   has_many :favorites
-  has_many :favorite_articles, through: favorites
+  # has_many :favorite_articles, through: favorites
 
   validate :email, "is required", -> (user : User) do
     (email = user.email) ? !email.empty? : false
