831dd8972a96a86515695ea8747f364e8ebf3562
ChangJoo Park <pcjpcj2@gmail.com>
2018-01-31 14:00:04 +0900

    fix #6 : fix alert layout and hide when after 2 seconds

--- src/assets/javascripts/main.js
+++ src/assets/javascripts/main.js
@@ -1,1 +1,13 @@
 import Amber from 'amber'
+
+const removeAlert = (time) => {
+  const $alert = document.querySelector('.alert')
+  if ($alert) {
+    setTimeout(() => {
+      $alert.remove()
+    }, time);
+  }
+}
+
+// Remove Alert after 2 seconds
+removeAlert(2000)
